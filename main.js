3fc5b84d4833e6a75c73229a3270fd01af287704
Norush <a123>
2023-11-26 13:05:39 +0800

    feat: base Light (各种灯光的使用)

--- main.js
+++ main.js
@@ -1,17 +1,9 @@
 import "./public/style/style.css"
-// import geometry from "./pages/geometry"
-// import atemplate from "./pages/atamplate"
-// import transforms from "./pages/base-transforms-objects"
-// import camera from "./pages/base-camera"
-// import fullScreen from "./pages/base-full-screen"
-// import baseGeometry from "./pages/base-geometry"
-// import controlsOption from "./pages/base-debug-ui"
-// import textures from "./pages/base11-textures"
-// import N3Keyboard from "./pages/case/N3-Keyboard"
-import Materials from "./pages/base12-materials"
+// import geometry from
+import BaseDemo from "./pages/base13-text"
 
 // # Base
-Materials()
+BaseDemo()
 
 // Case
 // N3Keyboard()
