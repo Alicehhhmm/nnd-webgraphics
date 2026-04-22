bef5261a2505cae8a71d477b3bc090c8c8b9111c
Alicehhhmm <a007aplum@gmail.com>
2026-04-22 08:23:33 +0800

    fix:  explame main

--- packages/framework-threejs/main.js
+++ packages/framework-threejs/main.js
@@ -1,9 +1,35 @@
-import './public/style/style.css'
-// import geometry from
-import BaseDemo from './pages/base14-lights'
+// All framework modules exports
+// Basics
+import BaseAnimations from './base-animations'
+import BaseCamera from './base-camera'
+import BaseDebugUI from './base-debug-ui'
+import BaseFullScreen from './base-full-screen'
+import BaseGeometry from './base-geometry'
+import BaseTransformsObjects from './base-transforms-objects'
 
-// # Base
-BaseDemo()
+// Advanced
+import BaseTextures from './base11-textures'
+import BaseMaterials from './base12-materials'
+import BaseText from './base13-text'
+import BaseLights from './base14-lights'
 
-// Case
-// N3Keyboard()
+// Other modules
+import Geometry from './geometry'
+
+// Export all modules
+export {
+  BaseAnimations,
+  BaseCamera,
+  BaseDebugUI,
+  BaseFullScreen,
+  BaseGeometry,
+  BaseTransformsObjects,
+  BaseTextures,
+  BaseMaterials,
+  BaseText,
+  BaseLights,
+  Geometry,
+}
+
+// Auto run the example module
+BaseAnimations()
